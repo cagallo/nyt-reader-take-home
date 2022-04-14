692e3049f2979e69e5ed1c9feb2d6b456739cea2
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-13 17:09:13 -1000

    Change caption tag to p

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -10,9 +10,9 @@ const ArticlePage = () => {
         <h2>{article.title}</h2>
         <p className="byline">{article.byline}</p>
         <img className="hero-image" src={article.multimedia[0].url} alt={article.multimedia[0].caption} />
-        <caption>{article.multimedia[0].caption}</caption>
+        <p className='article-caption'>{article.multimedia[0].caption}</p>
         <p className="article-text">{article.abstract} ...</p>
-        <a  href={article.url} target="_blank" rel="noreferrer"><button className="read-more-button">Read more</button></a>
+        <a  href={article.url} target="_blank" rel="noreferrer"><button className="read-more-button">Read more at NYTimes</button></a>
       </div>
   )
 }
