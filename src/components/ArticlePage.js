8dc2f8261a061442f188ee5277531621224b68b4
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-14 09:51:37 -0600

    Add classNames for styling purposes

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,18 +1,23 @@
-import { useLocation} from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
+import '../styles/ArticlePage.css'
 
 
 const ArticlePage = () => {
   const location = useLocation()
+  const navigate = useNavigate();
   const article  = location.state
 
   return ( 
       <div className="single-article-container">
-        <h2>{article.title}</h2>
+        <h2 className='single-page-title'>{article.title}</h2>
         <p className="byline">{article.byline}</p>
         <img className="hero-image" src={article.multimedia[0].url} alt={article.multimedia[0].caption} />
         <p className='article-caption'>{article.multimedia[0].caption}</p>
         <p className="article-text">{article.abstract} ...</p>
-        <a  href={article.url} target="_blank" rel="noreferrer"><button className="read-more-button">Read more at NYTimes</button></a>
+        <div className='button-container'>
+          <a  href={article.url} target="_blank" rel="noreferrer"><button className="read-more-button">Read more at NYTimes</button></a>
+          <button onClick={() => navigate('/')}>Return Home</button>
+        </div>
       </div>
   )
 }
