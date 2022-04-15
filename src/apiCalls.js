f21439ce9c74171083fd818a7d5d03f26deacf6b
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-15 10:09:15 -0600

    Remove console logs"

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,6 @@
 import { handleResponse } from './utils'
 
 export const getArticlesByCategory = async(name) => {
-  console.log(process.env.REACT_APP_NYT_KEY)
   try {
   let response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${name}.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
   let data = await handleResponse(response)
